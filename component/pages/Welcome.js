0d0474d4c487b6ca6074310dc52154197c51ee0a
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-10-15 10:45:29 +0200

    adaptive layouts

--- component/pages/Welcome.js
+++ component/pages/Welcome.js
@@ -1,37 +1,83 @@
-import React from 'react';
-import {StyleSheet, Text, View, Button, Image} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    StyleSheet,
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Image,
+    Dimensions,
+    ScrollView,
+    KeyboardAvoidingView
+} from 'react-native';
 import Card from '../widgets/Card';
 import Colors from '../constants/colors'
 
 const WelcomePage = props => {
+    const [ buttonWidth, setButtonWidth ] = useState(Dimensions.get('window').width / 4);
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setButtonWidth(Dimensions.get('window').width / 4);
+        };
+
+        Dimensions.addEventListener('change', updateLayout);
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+        };
+    });
+
     return (
+        <ScrollView>
+            <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
+                <TouchableWithoutFeedback
+                    onPress={() => {
+                        Keyboard.dismiss();
+                    }}
+                >
         <View style={styles.screen}>
-            <Text style={styles.headerTitle}>{props.date}</Text>
-            <Image source={require('../../assets/images/logo.png')}
-                   style={styles.logoImage}
-                   resizeMode='cover'
-            />
+            {/*<Text style={styles.headerTitle}>{props.date}</Text>*/}
+            <View style={styles.imgContainer}>
+                <Image source={require('../../assets/images/logo.png')}
+                       style={styles.image}
+                       resizeMode="cover" />
+            </View>
             <Card style={styles.inputContainer}>
-                <View style={styles.btnContainer}>
-                    <View style={styles.btn}><Button title="Signin" color={Colors.deepPrimaryColor}/></View>
-                    <View style={styles.btn}><Button title="Signup" color={Colors.deepPrimaryColor}/></View>
+                <View style={styles.buttonContainer}>
+                    <View style={{width: buttonWidth}}><Button title="Signin" color={Colors.deepPrimaryColor}/></View>
+                    <View style={{width: buttonWidth}}><Button title="Signup" color={Colors.deepPrimaryColor}/></View>
                 </View>
             </Card>
         </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </ScrollView>
     )
 }
 const styles = StyleSheet.create({
     btn: {
-        width: 100,
+        width: Dimensions.get('window').width / 2,
         color: 'black'
+    },
+    imgContainer: {
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        overflow: 'hidden',
+        marginVertical: Dimensions.get('window').height / 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     },
     helpText: {
         textAlign: 'center',
         marginBottom: 10
     },
     inputContainer: {
-        width: 300,
-        maxWidth: '80%',
+        width: '80%',
+        // maxWidth: '80%',
+        maxWidth: '95%',
+        minWidth: 300,
         alignItems: 'center'
     },
     screen: {
@@ -39,7 +85,7 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center'
     },
-    btnContainer: {
+    buttonContainer: {
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
@@ -48,12 +94,6 @@ const styles = StyleSheet.create({
     headerTitle: {
        marginVertical: 10,
        // fontFamily: 'open-sans-bold'
-    },
-    logoImage: {
-        width: '100%',
-        height: 320,
-        marginTop: 80,
-        marginBottom: 80
     }
 });
 export default WelcomePage;
