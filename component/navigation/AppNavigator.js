3783b54868bd4ca143fbbb2cae7c649a44c8f4df
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-10-18 10:46:17 +0200

    added bottom navigator fav icon

--- component/navigation/AppNavigator.js
+++ component/navigation/AppNavigator.js
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator, } from 'react-navigation';
 
 import SignUpPage from '../pages/SignUp';
 import SignInPage from '../pages/SignIn';
@@ -10,8 +10,13 @@ import CategoryWashScreen from "../pages/CategoryWashScreen"
 
 import Colors from '../constants/colors';
 import WashDetails from "../pages/WashDetails";
+import { Ionicons } from '@expo/vector-icons';
+import FavoritesScreen from "../FavoritesScreen";
+import React from "react";
+import ProfileScreen from "../ProfileScreen";
+import OrderScreen from "../OrdersScreen";
 
-const AppNavigator = createStackNavigator(
+const WahesNavigator = createStackNavigator(
     {
         WelcomePage: WelcomePage,
         Categories: {
@@ -38,4 +43,59 @@ const AppNavigator = createStackNavigator(
     }
 );
 
-export default createAppContainer(AppNavigator);
+const SpinnsSiteNavigator = createBottomTabNavigator(
+    {
+        Washes: {
+            screen: WahesNavigator,
+            navigationOptions: {
+                tabBarIcon: tabInfo => {
+                    return (
+                        <Ionicons name="ios-shirt" md="md-sync" size={25} color={tabInfo.tintColor}/>
+                    );
+                }
+            }
+        },
+        Favorites: {
+            screen: FavoritesScreen,
+            navigationOptions: {
+                tabBarLabel: 'Favorites',
+                tabBarIcon: tabInfo => {
+                    return (
+                        <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+                    );
+                }
+            }
+        },
+        Orders: {
+            screen: OrderScreen,
+            navigationOptions: {
+                tabBarLabel: 'Order',
+                tabBarIcon: tabInfo => {
+                    return (
+                        <Ionicons name="ios-calendar" size={25} color={tabInfo.tintColor} />
+                    );
+                }
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            navigationOptions: {
+                tabBarLabel: 'Profile',
+                tabBarIcon: tabInfo => {
+                    return (
+                        <Ionicons name="ios-person" size={25} color={tabInfo.tintColor} />
+                    );
+                }
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: Colors.primaryColor
+        }
+    }
+);
+
+
+
+export default createAppContainer(SpinnsSiteNavigator);
