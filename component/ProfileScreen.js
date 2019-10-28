3cd32beb390cd76a21f2e1098b0d59ae51a4660d
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-10-28 18:47:17 +0200

    display profile screen splash image

--- component/ProfileScreen.js
+++ component/ProfileScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import HeaderButton from './HeaderButton';
+import Colors from "./constants/colors";
 
 const ProfileScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Filters Screen!</Text>
+            <Image source={require('../assets/images/spinns-logo-mobile.jpg')} style={styles.image}
+                   resizeMode="center"/>
         </View>
     );
 };
@@ -32,8 +34,14 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
-    }
+        alignItems: 'center',
+        backgroundColor: Colors.cleanYellow
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+
 });
 
 export default ProfileScreen;
